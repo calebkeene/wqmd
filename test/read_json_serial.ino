686f8618dfcea7c5496424bb90eccc1ff789e6c3
calebkeene <caleb@hashbang.co.nz>
2015-09-10 19:44:02 +1200

    update Serial read function to v2 API :boar:

--- test/read_json_serial.ino
+++ test/read_json_serial.ino
@@ -1,7 +1,6 @@
-#include <ArduinoJson.h>
 
 void setup(){
-  Serial.begin(115200);
+  Serial.begin(9600);
 }
 
 void loop(){
@@ -9,20 +8,32 @@ void loop(){
 }
 
 void readSerial(){
-  
-  char json[150]; // Allocate some space for the string
+
+  /* ---------- Updated to API V2 ----------
+
+    two possible commands from the phone:
+    Test -> return one measurement
+    RetrieveData -> send all recent data
+    Status -> return status (could be error, i.e. nodata)
+
+    Since Serial read takes one char at a time, and this may not be delimited with a null
+    character, use ASCII representation (of command from phone)
+    (Can do this because a char in C is equivalent to an 8 bit int)
+
+    decimal representation of commands:
+
+    Test = 84+101+115+116 = 416
+    RetrieveData = 082 101 116 114 105 101 118 101 068 097 116 097 -> sum = 1216
+    Status = 083 116 097 116 117 115 -> sum = 644
+
+    */
+  int cmd = 0;
+
   if (Serial.available()){
-    
-    char currChar; // store current character being read
-    byte index = 0; // Index into array; where to store the character
     unsigned long start_time = millis();
 
     while(Serial.available() > 0){
-      if(index < (sizeof(json)-1)){ // keep within array index bounds
-        currChar = Serial.read(); // Read char
-        json[index] = currChar; 
-        index++; // add to String
-        json[index] = '\0'; // Null terminate the string
+        cmd += Serial.read(); // Read char, add to ASCII sum
       }
       // if greater than 10 seconds have passed, break out (timeout)
       if((millis() - start_time) > 10000){
@@ -30,17 +41,23 @@ void readSerial(){
       }
     }
   }
-  // now parse String (char array) to JSON object
-  StaticJsonBuffer<16> jsonBuffer;
-  JsonObject& root = jsonBuffer.parseObject(json);
 
-  if (!root.success()){
-    Serial.println("parseObject() failed");
-    return;
+  if(cmd == 416){// Test
+  //take single sample, return it
+  Serial.println(sendSingleSample()); // will need to define single sample function
+  
+  }
+  else if(cmd == 1216){// RetrieveData
+  //send all recent data
+    sendAllData();
+  }
+  else if(cmd == 644){// Status
+    /*
+    poll status, return to phone
+    check if there is data on SD, if not send (JSON) -> "{"status":"nodata"}"
+    perform some kind of self check, if all systems OK, send -> "{"status":"ready"}"
+    */
   }
 
-  const char* cmd = root["cmd"];
- 
- 	//echo back the JSON contents to Serial 
  	Serial.println(cmd);
 }
